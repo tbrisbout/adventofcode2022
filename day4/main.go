9abc878a6371ddb2c3e19f914c2f508f688e0278
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2023-01-11 23:52:52 +0100

    Add day4 part2

--- day4/main.go
+++ day4/main.go
@@ -21,6 +21,24 @@ func countContainedRanges(input string) (sum int) {
 	return sum
 }
 
+func between(n, lower, upper int) bool {
+	return lower <= n && n <= upper
+}
+
+func countOverlappingRanges(input string) (sum int) {
+	for _, ranges := range rangePattern.FindAllStringSubmatch(input, -1) {
+		n := pkg.Map(ranges[1:], pkg.MustAtoi)
+
+		a1, a2, b1, b2 := n[0], n[1], n[2], n[3]
+		if between(b1, a1, a2) || between(b2, a1, a2) || between(a1, b1, b2) || between(a2, b1, b2) {
+			sum++
+		}
+	}
+
+	return sum
+}
+
 func main() {
 	fmt.Println("Part1:", countContainedRanges(mainInput))
+	fmt.Println("Part1:", countOverlappingRanges(mainInput))
 }
--- day4/main_test.go
+++ day4/main_test.go
@@ -19,3 +19,20 @@ func TestCountContainedRanges(t *testing.T) {
 		t.Fatalf("expected %d but got %d", want, got)
 	}
 }
+func TestCountOverlappingRanges(t *testing.T) {
+	input := `
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+`
+
+	got := countOverlappingRanges(input)
+	want := 4
+
+	if got != want {
+		t.Fatalf("expected %d but got %d", want, got)
+	}
+}
